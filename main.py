f499ceca0404ebdc0ecd267aa5e5f2cf29b837f0
ashen <jayasinghe.ashen@gmail.com>
2021-11-16 19:54:07 +0530

    adding skeleton methods

--- main.py
+++ main.py
@@ -10,7 +10,16 @@ def main():
     tree_tuple = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
 
     # convert tuple to Tree Node Object
-    tree = Node.parse_tuple(tree_tuple)
+    node = Node.parse_tuple(tree_tuple)
+
+    node
+
+    node.display_keys('     ')
+    node.max_depth()
+    node.min_depth()
+    node.size()
+    node.diameter()
+    node.is_bst()
 
 
 if __name__ == '__main__':
