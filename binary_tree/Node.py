eb39bbf7aaf90db6ab699ac18c302a0080e86d81
ashen <jayasinghe.ashen@gmail.com>
2021-11-17 00:41:08 +0530

    adding tree to tuple, tuple to tree conversion

--- binary_tree/Node.py
+++ binary_tree/Node.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 class Node:
     def __init__(self, key) -> None:
         self.key = key
@@ -21,20 +24,36 @@ class Node:
 
     # tree to tuple
     def to_tuple(self):
-        pass
+        if self is None:
+            return None
+
+        if self.left is None and self.right is None:
+            return self.key
+
+        return (Node.to_tuple(self.left), self.key, Node.to_tuple(self.right))
 
     # is binary search tree
     def is_bst(self):
         pass
 
     def __repr__(self):
-        pass
+        return "Binary Tree: < {} >".format(self.to_tuple())
 
     def __str__(self):
-        pass
+        return "Binary Tree: < {} >".format(self.to_tuple())
 
     # tuple to Tree Node Obj
-
+    # ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
     @staticmethod
     def parse_tuple(tuple):
-        pass
+        if tuple is None:
+            node = None
+        if isinstance(tuple, Tuple) and len(tuple) == 3:
+            # print(f"parse_tuple().tuple exist: {tuple}")
+            node = Node(tuple[1])  # key
+            node.left = Node.parse_tuple(tuple[0])
+            node.right = Node.parse_tuple(tuple[2])
+        else:
+            node = Node(tuple)
+        # print(Node.to_tuple(node))
+        return node
--- main.py
+++ main.py
@@ -1,26 +1,43 @@
 from binary_tree.Node import Node
 
 
-def main():
+def binary_tree(tuple):
+    if tuple:
+        # convert tuple to Tree
+        node = Node.parse_tuple(tuple)
+        print(node.__str__)
+
+        if node:
+            # node.display_keys('     ')
+            # node.max_depth()
+            # node.min_depth()
+            # node.size()
+            # node.diameter()
+            # node.is_bst()
+            # node.to_tuple()
+            pass
+        else:
+            print(f"Tree found None! Invalid Tuple passed!")
+
+
+if __name__ == '__main__':
+    tuple_list = []
+
     #          2
     #    3            5
     # 1     n      3      7
     #            n   4  6    8
     #
-    tree_tuple = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
 
-    # convert tuple to Tree Node Object
-    node = Node.parse_tuple(tree_tuple)
+    # test scenarios
+    test_tuple1 = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
+    test_tuple2 = (2)
+    test_tuple3 = (1, 2)  # invalid
 
-    node
+    tuple_list.append(test_tuple1)
+    tuple_list.append(test_tuple2)
+    tuple_list.append(test_tuple3)
 
-    node.display_keys('     ')
-    node.max_depth()
-    node.min_depth()
-    node.size()
-    node.diameter()
-    node.is_bst()
-
-
-if __name__ == '__main__':
-    main()
+    for test_no, tuple in enumerate(tuple_list):
+        binary_tree(tuple)
+        print(f"TEST {test_no}: TUPLE <{tuple}> ===> Finished")
